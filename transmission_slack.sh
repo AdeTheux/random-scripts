b3ed8b99a7b6e2e167fb62496ab1d835a4acd320
Arnaud de Theux <a.detheux@mac.com>
2021-02-05 15:48:55 +0100

    Migrating from Pushover to Slack

--- transmission_slack.sh
+++ transmission_slack.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
-#-F "sound=cashregister" \ 
+#!/bin/bash
 
-curl -s \
-	-F "token=abc" \
-	-F "user=xyz" \
-	-F "title=✅ Transmission download finished" \
-	-F "message=$TR_TORRENT_NAME ($TR_TIME_LOCALTIME)" \
-	http://api.pushover.net/1/messages > /dev/null
+CHANNEL="#household"
+MSG="🗳 Torrent download completed: $TR_TORRENT_NAME ($TR_TIME_LOCALTIME)"
+
+PAYLOAD="payload={\"channel\": \"$CHANNEL\", \"text\": \"$MSG\"}"
+HOOK=https://hooks.slack.com/services/x/y/z
+
+curl -X POST --data-urlencode "$PAYLOAD" "$HOOK"
